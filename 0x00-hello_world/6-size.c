fa393686b911ddff7b780212f2bcb60d45dcf0be
Daniel Brobbey <danielbrobbey1@gmail.com>
2023-02-16 14:53:15 -0800

    whe

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,14 +9,14 @@ int main(void)
 {
 	char charity;
 	int inte;
-	long int longint;
-	long long int longlong;
+	long int lot;
+	long long int l;
 	float buoyancy;
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(charity));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(inte));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(longint));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(longlong));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(lot));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(l));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(buoyancy));
 	return (0);
 }
