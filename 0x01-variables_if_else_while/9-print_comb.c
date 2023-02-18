7908cfd01b87465612fa0d8e671ace7886821a4d
Daniel Brobbey <danielbrobbey1@gmail.com>
2023-02-18 14:07:37 -0800

    wow

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int index = 0;
-	while(index < 10)
+
+	while (index < 10)
 	{
 		putchar(index + '0');
 		if(index < 9)
