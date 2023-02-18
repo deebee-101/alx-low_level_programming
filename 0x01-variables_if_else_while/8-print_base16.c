2b4942412c86faca1c1e03d85f37f5ec56643de6
Daniel Brobbey <danielbrobbey1@gmail.com>
2023-02-18 14:03:27 -0800

    wow

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int index = 0;
 	char num[10] = "0123456789";
 	char alp[6] = "abcdef";
 
-	while(index < 10)
+	while (index < 10)
 	{
 		putchar(num[index]);
 		index++;
 	}
 	index = 0;
-	while(index < 6)
+	while (index < 6)
 	{
 		putchar(alp[index]);
 		index++;
