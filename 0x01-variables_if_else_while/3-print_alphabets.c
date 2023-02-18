48500f42ea6bc5c97c6cf14964c27f4319b6655b
Daniel Brobbey <danielbrobbey1@gmail.com>
2023-02-18 13:29:14 -0800

    print

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Description: 'alpha'
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int index = 0;
 	char alph[26] = "abcdefghijklmnopqrstuvwxyz";
 	char ABET[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-	while(index < 26)
+	while (index < 26)
 	{
 		putchar(alph[index]);
 		index++;
 	}
 	index = 0;
-	while(index < 26)
+	while (index < 26)
 	{
 		putchar(ABET[index]);
 		index++;
