419282de0ca632ca25eb7b8bdd8d60af5c816531
Daniel Brobbey <danielbrobbey1@gmail.com>
2023-02-18 13:35:14 -0800

    damn

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
 	int index = 0;
 
-	while(index < 26)
+	while (index < 26)
 	{
-		if(alpha[index] == 'e' || alpha[index] == 'q')
+		if (alpha[index] == 'e' || alpha[index] == 'q')
 		{
 			index++;
 			continue;
@@ -16,7 +22,7 @@ int main(void)
 		{
 			putchar(alpha[index++]);
 		}
-	
+
 	}
 	putchar('\n');
 	return (0);
