d80f3812129c16b9a00f76e86c9b5745277630ae
Daniel Brobbey <danielbrobbey1@gmail.com>
2023-02-18 13:22:37 -0800

    print

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Description: 'print alpha'
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char alpha[26] =  "abcdefghijklmnopqrstuvwxyz";
 	int index = 0;
 
-	while(index < 26)
+	while (index < 26)
 	{
 		putchar(alpha[index]);
 		index++;
