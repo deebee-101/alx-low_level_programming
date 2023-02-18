b75c38f0c7c51b46343340fe46799686946727f2
Daniel Brobbey <danielbrobbey1@gmail.com>
2023-02-18 12:33:52 -0800

    betty again

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,16 +2,15 @@
 #include <time.h>
 #include <stdio.h>
 /**
- *
- * find negatives
+ *find negatives
+ * 
  *
  *
  */
 
 /* betty style doc for function main goes there */
 int main(void)
-{
-	
+{	
 	int n;
 
 	srand(time(0));
