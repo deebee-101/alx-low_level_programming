01da703fb0723cd8a575204bf76ddc49954302e1
Daniel Brobbey <danielbrobbey1@gmail.com>
2023-02-18 14:37:11 -0800

    well

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'com3'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main() 
+{
+	int index = 0;
+
+	while (index < 10)
+        {
+		int india = index +1;
+		while (india  < 10)
+		{
+			putchar(index + '0');
+			putchar(india + '0');
+			if (index < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			india++;
+		}
+		index++;
+	}
+	putchar('\n');
+	return 0;
+}
