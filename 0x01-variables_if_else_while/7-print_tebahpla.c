3bdd83132a67eadf4dc3e46a5685f0df6187cfa1
Daniel Brobbey <danielbrobbey1@gmail.com>
2023-02-18 13:52:36 -0800

    betty

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ * 
+ * Description: 'the program's description'
+ * 
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char alpa[26] = "abcdefghijklmnopqrstuvwxyz";
 	int index = 26;
-	while(index > 0)
+
+	while (index > 0)
 	{
 		index--;
 		putchar(alpa[index]);
