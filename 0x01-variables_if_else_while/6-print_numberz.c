44ad246a3a9ee83836530f808ba63c55b34d4220
Daniel Brobbey <danielbrobbey1@gmail.com>
2023-02-18 13:48:17 -0800

    zzzz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int index = 0;
-	while(index < 10)
+
+	while (index < 10)
 	{
 		putchar(index + '0');
-		index++;
-			
+		index++;		
 	}
 	putchar('\n');
 	return (0);
