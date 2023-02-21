649b7ba0e539149925dd296066a31d4a2f7c7fca
Daniel Brobbey <danielbrobbey1@gmail.com>
2023-02-21 14:16:38 -0800

    why

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * use putchar
+ *
+ * Return: On success 0
+ */
+int main (void)
+{
+	char to_print[8] = "_putchar";
+	int index = 0;
+
+	while (index < 8)
+	{
+		_putchar (to_print[index]);
+		index++;
+	}
+	_putchar ('\n');
+	return (0);
+}
+
+int _putchar (char c)
+{
+	return (write(1, &c, 1));
+}
