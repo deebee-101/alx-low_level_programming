6dc65d6599c6175847543f29f29e30ee6e38204c
Daniel Brobbey <danielbrobbey1@gmail.com>
2023-02-21 15:22:30 -0800

    create better exercises

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - alphabet
+ *
+ * Desciption: 'print alpha'
+ *
+ * return: On success 0
+ */
+
+void print_alphabet(void)
+{
+	char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
+	int index = 0;
+
+	while (index < 26)
+	{
+		_putchar (alpha[index]);
+		index++;
+	}
+	_putchar ('\n');
+}
