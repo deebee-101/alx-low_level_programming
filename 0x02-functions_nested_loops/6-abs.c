fb161aa5ff80a220113563449668a685303c06b2
Daniel Brobbey <danielbrobbey1@gmail.com>
2023-02-22 02:18:58 -0800

    abd

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * int _abs - absolutely
+ * @n: input parameter
+ *
+ * Description: 'print absolutes'
+ *
+ * Return: Nothing
+ */
+
+int _abs(int n)
+{
+	int absolutely = abs(n);
+	_putchar (absolutely + '0');
+	return (0);
+}
+
+
