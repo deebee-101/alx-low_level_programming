40fd1697f6b95e5d785d7302859c93786891a2c2
Daniel Brobbey <danielbrobbey1@gmail.com>
2023-02-27 15:06:30 -0800

    wow

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - half
+ * @str: variable
+ *
+ * Description: 'half'
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int k = strlen(str);
+	int j = k / 2;
+	int n = (k - 1) / 2;
+
+	if (k % 2 == 0)
+	{
+		while (str[j] != '\0') 
+		{
+			_putchar(str[j]);
+			j++;
+		}
+	}
+	else 
+	{
+		while  (str[n] != '\0')
+		{
+			_putchar(str[n]);
+		}
+	}
+	_putchar('\n');
+}
