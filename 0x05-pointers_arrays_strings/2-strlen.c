ec5a9ab51e29904369aba2d72d18afc9e0870a47
Daniel Brobbey <danielbrobbey1@gmail.com>
2023-02-27 10:00:54 -0800

    lenght

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - string lenght
+ * @s: string
+ *
+ * Description: 'stringlength'
+ *
+ * Return: integer string length
+ */
+
+int _strlen(char *s)
+{
+	return(strlen(s));
+}
