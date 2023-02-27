921dba363ad849ddb49fb1c8b6834a6c4922d58a
Daniel Brobbey <danielbrobbey1@gmail.com>
2023-02-27 14:10:29 -0800

    wowo

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - reverse
+ * @s - parameter
+ *
+ * Description: 'reverse'
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i = strlen(s) - 1;
+
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
