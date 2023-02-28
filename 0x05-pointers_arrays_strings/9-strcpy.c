4c9b83f4eea7b0a9a5c0b2c85c8c984b151ed2db
Daniel Brobbey <danielbrobbey1@gmail.com>
2023-02-28 09:57:42 -0800

    copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcpy - copy
+ * @dest: destination
+ * @src: source
+ *
+ * Description: 'copy'
+ *
+ * Return: nope
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+	char tempo;
+
+	while (src[i] != '\0')
+	{
+		tempo = src[i];
+		dest[i] = tempo;
+		i++;
+	}
+	return (dest);
+}
