777105504fe1261051791ad709327d7c0a5364e8
Daniel Brobbey <danielbrobbey1@gmail.com>
2023-02-28 09:42:24 -0800

    rev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - reverse
+ * @s: some string
+ *
+ * Description: 'reverse'
+ *
+ * Return: nothing
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+	int j = strlen(s) - 1;
+	char t;
+
+	while (i < j)
+	{
+		t = s[i];
+		s[i] = s[j];
+		s[j] = t;
+		i++;
+		j--;
+	}
+
+	
+}
