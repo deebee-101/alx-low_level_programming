aeea88e5f280e0c928101ebdca112f4aeca7d12e
Daniel Brobbey <danielbrobbey1@gmail.com>
2023-03-05 15:25:24 -0800

    break

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * *_strpbrk - first
+ * @s: string
+ * @accept: look here
+ *
+ * Description: 'finds first'
+ *
+ * Return: char
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0;
+	char *b;
+
+	while (s[i] !='\0')
+	{
+		int j = 0;
+
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				b = &s[i];
+				return (b);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (NULL);
+}
