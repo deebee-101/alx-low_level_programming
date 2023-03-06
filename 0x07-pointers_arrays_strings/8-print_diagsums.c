9e4e70a9ae1b6fd0b0028ce1788fb74289d2e939
Daniel Brobbey <danielbrobbey1@gmail.com>
2023-03-06 02:59:05 -0800

    horrible

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagsums - sum diag
+ * @a: aray
+ * @size: its size
+ *
+ * Description: 'sum size'
+ *
+ * Return: nada
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int j;
+	int sum1 = 0;
+	int sum2 = 0;
+	int length = size * size;
+
+	for (i = 0; i < length; i += size + 1)
+	{
+		sum1 += a[i];
+	}
+	for (j = size - 1; j < length; j += size - 1)
+	{
+		sum2 += a[j];
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
